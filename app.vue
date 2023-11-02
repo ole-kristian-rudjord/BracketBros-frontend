e299d36453894b6b8a01aae10321c53275f28f04
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-02 19:19:47 +0100

    Added components and tags for testing

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+  import { toast } from 'vue3-toastify';
+  import { defaultToastOptions } from '~/constants';
+
+  const testToast = (
+    text: string,
+    type: 'info' | 'success' | 'warning' | 'error'
+  ) => {
+    toast[type](text, defaultToastOptions[type]);
+  };
+</script>
+
 <template>
-  <div>Hello</div>
+  <v-btn @click="testToast('this is info', 'info')">Hello</v-btn>
+  <div>testing normal</div>
+  <div class="font-family--mono">testing mono</div>
+  <v-btn icon="fa-brands fa-discord" variant="tonal"></v-btn>
 </template>
