3251aebb41f8588948550a617cb7b79c85a268e5
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-06 20:52:04 +0100

    Experimenting with displayment of posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
   import { placeholder_post } from '~/constants';
 
+  const posts = ref();
+
   onMounted(async () => {
     const { data, error } = await getAllPosts();
     if (error) {
       console.log(error);
     } else {
       console.log(data);
+      posts.value = data;
     }
   });
 </script>
 
 <template>
-  <div class="d-flex justify-center w-100 py-8 px-4">
+  <div class="d-flex flex-column align-center w-100 py-12 px-4">
     <post :propPost="placeholder_post"></post>
+    <!-- <post
+      v-for="(post, index) in posts"
+      :key="index"
+      :propPost="placeholder_post"
+      class="mb-12"
+    ></post> -->
   </div>
 </template>
