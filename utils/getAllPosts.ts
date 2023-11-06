03efae87a1531da01be5b44bf4eaf39046ca8b89
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-06 20:35:51 +0100

    First commit

---
+++ utils/getAllPosts.ts
@@ -0,0 +1,22 @@
+export default async () => {
+  try {
+    const response = await fetch('https://localhost:7205/api/Post/posts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add Authorization header if your endpoint requires it
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return { data, error: null };
+    } else {
+      console.error('Error fetching posts:', response.statusText);
+      return { data: null, error: response.statusText };
+    }
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    return { data: null, error };
+  }
+};
