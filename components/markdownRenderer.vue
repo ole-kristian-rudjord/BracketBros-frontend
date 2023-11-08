5a5d3e501573cbc445f38e21b865414c33c4b3b4
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-08 19:34:17 +0100

    First commit

---
+++ components/markdownRenderer.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+  import { useTheme } from 'vuetify/lib/framework.mjs';
+
+  const theme = useTheme();
+
+  const onSurfaceVariant = ref(
+    theme.current.value.colors['on-surface-variant']
+  );
+
+  watch(
+    () => theme.current.value.colors['on-surface-variant'],
+    (newColor) => {
+      onSurfaceVariant.value = newColor;
+    },
+    { immediate: true }
+  );
+
+  defineProps<{ content: string }>();
+</script>
+
+<template>
+  <div v-html="$mdRenderer.render(content)"></div>
+</template>
+
+<style scoped lang="scss">
+  :deep() {
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    hr,
+    ul,
+    pre {
+      margin-bottom: 12px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    ul {
+      margin-left: 24px;
+    }
+
+    pre {
+      padding: 10px 12px;
+      font-size: 14px;
+      white-space: pre-wrap;
+      background-color: v-bind(onSurfaceVariant);
+      border-radius: 8px;
+    }
+
+    :not(pre) code {
+      padding: 1px 4px;
+      background-color: v-bind(onSurfaceVariant);
+      border-radius: 4px;
+    }
+  }
+</style>
