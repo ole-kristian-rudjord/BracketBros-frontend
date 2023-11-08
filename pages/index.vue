206c646e6b6c97a684b54658681398c218c30186
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-08 15:43:01 +0100

    Switched posts from index page to posts page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,3 @@
-<script setup lang="ts">
-  const posts = ref<post[]>();
+<script setup lang="ts"></script>
 
-  onMounted(async () => {
-    const { data, error } = await getAllPosts();
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-      posts.value = data;
-    }
-  });
-</script>
-
-<template>
-  <div class="d-flex flex-column align-center w-100 py-12 px-4">
-    <post
-      v-for="(post, index) in posts"
-      :key="index"
-      :propPost="post"
-      class="mb-12"
-    ></post>
-  </div>
-</template>
+<template>index</template>
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,3 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const posts = ref<post[]>();
 
-<template>posts</template>
+  onMounted(async () => {
+    const { data, error } = await getAllPosts();
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      posts.value = data;
+    }
+  });
+</script>
+
+<template>
+  <div class="d-flex flex-column align-center w-100 py-12 px-4">
+    <post
+      v-for="(post, index) in posts"
+      :key="index"
+      :propPost="post"
+      class="mb-12"
+    ></post>
+  </div>
+</template>
