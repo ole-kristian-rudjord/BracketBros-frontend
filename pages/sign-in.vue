3c4181ca73968e459a381572e621e625ca01ea76
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-13 19:20:02 +0100

    Added validation

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -3,6 +3,7 @@
     title: 'Login - BracketBros',
   });
 
+  const form = ref(false);
   const identifier = ref('');
   const password = ref('');
   const rememberMe = ref(false);
@@ -12,6 +13,10 @@
   const isLoading = ref(false);
 
   const error = ref<null | 'unauthorized' | 'unexpectedError'>(null);
+
+  const rules = {
+    required: (value: any) => !!value || 'Field is required',
+  };
 
   const login = async () => {
     isLoading.value = true;
@@ -52,57 +57,48 @@
             : 'Unexpected error when trying to sign in, please try again later.'
         "
       ></v-alert>
-      <v-form @submit.prevent="login">
-        <v-row>
-          <v-col cols="12">
-            <v-text-field
-              label="Email or username"
-              v-model="identifier"
-              variant="outlined"
-              hide-details
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field
-              label="Password"
-              v-model="password"
-              :type="showPassword ? 'text' : 'password'"
-              variant="outlined"
-              hide-details
-            >
-              <template v-slot:append-inner>
-                <v-icon
-                  :icon="
-                    showPassword ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye'
-                  "
-                  size="x-small"
-                  @click="showPassword = !showPassword"
-                ></v-icon>
-              </template>
-            </v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-checkbox
-              label="Remember me"
-              v-model="rememberMe"
-              false-icon="fa-regular fa-square"
-              true-icon="fa-regular fa-square-check"
-              hide-details
-            ></v-checkbox>
-          </v-col>
-          <v-col cols="12">
-            <v-btn
-              type="submit"
-              size="x-large"
-              variant="tonal"
-              block
-              class="text-body-1"
-              :loading="isLoading"
-            >
-              Sign in
-            </v-btn>
-          </v-col>
-        </v-row>
+      <v-form v-model="form" @submit.prevent="login">
+        <v-text-field
+          label="Email or username"
+          v-model="identifier"
+          variant="outlined"
+          :rules="[rules.required]"
+          class="mb-2"
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          :type="showPassword ? 'text' : 'password'"
+          variant="outlined"
+          :rules="[rules.required]"
+        >
+          <template v-slot:append-inner>
+            <v-icon
+              :icon="showPassword ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye'"
+              size="x-small"
+              @click="showPassword = !showPassword"
+            ></v-icon>
+          </template>
+        </v-text-field>
+        <v-checkbox
+          label="Remember me"
+          v-model="rememberMe"
+          false-icon="fa-regular fa-square"
+          true-icon="fa-regular fa-square-check"
+          hide-details
+          class="mb-5"
+        ></v-checkbox>
+        <v-btn
+          type="submit"
+          size="x-large"
+          variant="tonal"
+          block
+          class="text-body-1"
+          :disabled="!form"
+          :loading="isLoading"
+        >
+          Sign in
+        </v-btn>
       </v-form>
     </v-sheet>
   </div>
