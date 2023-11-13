9352b74f02346920d9b76639cd8cfe94b27d53f3
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-13 18:22:35 +0100

    First commit

---
+++ utils/loginUser.ts
@@ -0,0 +1,23 @@
+export default async (loginData: loginData) => {
+  try {
+    console.log('Trying to login');
+    const response = await fetch(`https://localhost:7205/api/Account/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(loginData),
+    });
+
+    if (response.ok) {
+      const data = await response.text();
+      return { data, error: null };
+    } else {
+      return { data: null, error: response.status };
+    }
+  } catch (error) {
+    console.error('Error login:', error);
+    return { data: null, error };
+  }
+};
