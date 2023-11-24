a3d4440113e09fedf4449a8e31626089bca41ee4
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-24 19:00:22 +0100

    Formatting

--- utils/getAllCategories.ts
+++ utils/getAllCategories.ts
@@ -1,12 +1,11 @@
 export default async () => {
-  const {data: categoriesData} = await genericFetch({
+  const { data: categoriesData } = await genericFetch({
     method: 'GET',
     url: 'https://localhost:7205/api/Post/GetCategories',
   });
 
-
-  categoriesData.forEach((category: { url: string; }) => {
-    category.url = category.url.replace("../", "https://localhost:7205/");
+  categoriesData.forEach((category: { url: string }) => {
+    category.url = category.url.replace('../', 'https://localhost:7205/');
   });
 
   return categoriesData;
