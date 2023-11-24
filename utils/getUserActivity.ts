fa6be55f1a50925d7810f9188ea2af1f287394bf
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-24 15:06:00 +0100

    Formatting

--- utils/getUserActivity.ts
+++ utils/getUserActivity.ts
@@ -1,14 +1,16 @@
 export default async (full: boolean = false) => {
-    const response = await genericFetch({
-        method: 'GET',
-        url: `https://localhost:7205/api/DashBoard/UserActivity/${full ? 'full' : ''}`,
-    });
-    if (response.error === null) {
-        if (!full)
-            sessionStorage.setItem('UserActivity', JSON.stringify(response.data));
-        return {data: response.data, error: null};
-    } else {
-        const error = 'Unable to fetch UserActivity';
-        return {data: null, error: error};
-    }
+  const response = await genericFetch({
+    method: 'GET',
+    url: `https://localhost:7205/api/DashBoard/UserActivity/${
+      full ? 'full' : ''
+    }`,
+  });
+  if (response.error === null) {
+    if (!full)
+      sessionStorage.setItem('UserActivity', JSON.stringify(response.data));
+    return { data: response.data, error: null };
+  } else {
+    const error = 'Unable to fetch UserActivity';
+    return { data: null, error: error };
+  }
 };
