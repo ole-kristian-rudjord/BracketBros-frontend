7af19e5dbb72a47dd432c39194d49918314c0a39
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-24 15:05:54 +0100

    Removed unused code

--- utils/loginUser.ts
+++ utils/loginUser.ts
@@ -12,27 +12,4 @@ export default async (loginData: loginData) => {
   } else {
     return { data: null, error: response.error, status: response.status };
   }
-  // try {
-  //   console.log('Trying to login');
-  //   const response = await fetch(`https://localhost:7205/api/Account/login`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     credentials: 'include',
-  //     body: JSON.stringify(loginData),
-  //   });
-
-  //   if (response.ok) {
-  //     const data = await response.text();
-  //     getUserActivity();
-  //     router.push('/user-activity');
-  //     return { data, error: null };
-  //   } else {
-  //     return { data: null, error: response.status };
-  //   }
-  // } catch (error) {
-  //   console.error('Error login:', error);
-  //   return { data: null, error };
-  // }
 };
--- utils/registerUser.ts
+++ utils/registerUser.ts
@@ -11,29 +11,4 @@ export default async (registerData: registerData) => {
   } else {
     return { data: null, error: response.error };
   }
-
-  // try {
-  //   console.log('Trying to login');
-  //   const response = await fetch(
-  //     `https://localhost:7205/api/Account/register`,
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       credentials: 'include',
-  //       body: JSON.stringify(registerData),
-  //     }
-  //   );
-
-  //   if (response.ok) {
-  //     const data = await response.text();
-  //     return { data, error: null };
-  //   } else {
-  //     return { data: null, error: response.status };
-  //   }
-  // } catch (error) {
-  //   console.error('Error login:', error);
-  //   return { data: null, error };
-  // }
 };
