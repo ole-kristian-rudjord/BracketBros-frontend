4ffd13f24acd6b1533eaa4c746127c08105ed075
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-24 11:20:41 +0100

    Formatting

--- pages/create-post.vue
+++ pages/create-post.vue
@@ -6,49 +6,54 @@
   let categories = ref<category[]>([]);
   let tags = ref<tag[]>([]);
 
-const loadData = async () => {
-  const { data: categoriesData } = await genericFetch('https://localhost:7205/api/Post/GetCategories', 'GET');
-  const { data: tags } = await genericFetch('https://localhost:7205/api/Post/GetTags', 'GET');
-  
- /* categories.value = categoriesData;
+  const loadData = async () => {
+    const { data: categoriesData } = await genericFetch(
+      'https://localhost:7205/api/Post/GetCategories',
+      'GET'
+    );
+    const { data: tags } = await genericFetch(
+      'https://localhost:7205/api/Post/GetTags',
+      'GET'
+    );
+
+    /* categories.value = categoriesData;
   tags.value = tags;*/
-
-};
-
-
+  };
 
   onMounted(() => {
     // if (!user.value) {
     //   router.push('/login');
     // }
     loadData();
-
   });
 
+  const form = ref(false);
+  const title = ref('');
+  const category = ref('');
+  const content = ref('');
 
-const form = ref(false);
-const title = ref('');
-const category = ref('');
-const content = ref('');
+  const isLoading = ref(false);
+  const error = ref<null | 'unexpectedError'>(null);
 
+  const rules = {
+    required: (value: string) => !!value || 'Field is required',
+    title: (value: string) => {
+      const titlePattern = /^[0-9a-zA-ZæøåÆØÅ \-/:/?/./!/#]{2,64}$/;
+      return (
+        titlePattern.test(value) ||
+        'The title can only contain numbers, letters or characters -:?.!, and must be between 2 to 64 characters.'
+      );
+    },
+    content: (value: string) => {
+      const contentPattern = /^.{2,4096}$/;
+      return (
+        contentPattern.test(value) ||
+        'The content must be between 2 to 4096 characters.'
+      );
+    },
+  };
 
-const isLoading = ref(false);
-const error = ref<null | 'unexpectedError'>(null);
-
-const rules = {
-  required: (value: string) => !!value || 'Field is required',
-  title: (value: string) => {
-    const titlePattern = /^[0-9a-zA-ZæøåÆØÅ \-/:/?/./!/#]{2,64}$/;
-    return titlePattern.test(value) || 'The title can only contain numbers, letters or characters -:?.!, and must be between 2 to 64 characters.';
-  },
-  content: (value: string) => {
-    const contentPattern = /^.{2,4096}$/;
-    return contentPattern.test(value) || 'The content must be between 2 to 4096 characters.';
-  }
-};
-
-
-const register = async () => {
+  const register = async () => {
     isLoading.value = true;
 
     // const post: post = {
@@ -69,14 +74,14 @@ const register = async () => {
 
     isLoading.value = false;
   };
-
-
 </script>
 
-<template>create post
+<template>
+  create post
 
-<nuxt-layout name="login-register">   <!-- Please change this -->
-  <v-form v-model="form" @submit.prevent="register">
+  <nuxt-layout name="login-register">
+    <!-- Please change this -->
+    <v-form v-model="form" @submit.prevent="register">
       <v-text-field
         label="Title"
         v-model="title"
@@ -85,7 +90,7 @@ const register = async () => {
         class="mb-3"
       ></v-text-field>
 
-        <v-select
+      <v-select
         label="Category"
         v-model="category"
         :items="categories"
@@ -93,8 +98,9 @@ const register = async () => {
         item-value="categoryId"
         variant="outlined"
         :rules="[rules.required]"
-        class="mb-3"></v-select>
-<!-- 
+        class="mb-3"
+      ></v-select>
+      <!-- 
        <v-select multiple
         label="Tags"
         v-model="tag"
@@ -105,13 +111,13 @@ const register = async () => {
         :rules="[rules.required]"
         class="mb-3"></v-select> -->
 
-
       <v-textarea
         label="Content"
         v-model="content"
         variant="outlined"
         :rules="[rules.required, rules.content]"
-        class="mb-3"></v-textarea>
+        class="mb-3"
+      ></v-textarea>
 
       <v-btn
         type="submit"
@@ -121,7 +127,8 @@ const register = async () => {
         class="text-body-1"
         :disabled="!form"
         :loading="isLoading"
-      >Create post</v-btn>
+        >Create post</v-btn
+      >
     </v-form>
   </nuxt-layout>
 </template>
