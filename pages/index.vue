6fdb236a983f54e20035b4313282e42c379c63d2
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-24 19:09:38 +0100

    Formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,41 @@
 <script setup lang="ts">
-import {toast} from "vue3-toastify";
-import {defaultToastOptions} from "~/constants";
+  import { toast } from 'vue3-toastify';
+  import { defaultToastOptions } from '~/constants';
 
-useHead({
-  title: 'BracketBros',
-});
+  useHead({
+    title: 'BracketBros',
+  });
 
-const availableCategories = ref<category[]>([]);
-const availableCategories_isLoading = ref(false);
+  const availableCategories = ref<category[]>([]);
+  const availableCategories_isLoading = ref(false);
 
+  onMounted(async () => {
+    // TODO: reroute user to login if not logged in
 
-onMounted(async () => {
-  // TODO: reroute user to login if not logged in
+    availableCategories_isLoading.value = true;
 
-  availableCategories_isLoading.value = true;
-
-  const categoriesData = await getAllCategories();
-  if (categoriesData) {
-    availableCategories.value = categoriesData;
-  } else {
-    toast.error(
+    const categoriesData = await getAllCategories();
+    if (categoriesData) {
+      availableCategories.value = categoriesData;
+    } else {
+      toast.error(
         'Error fetching categories from the database.',
         defaultToastOptions.error
-    );
-  }
-  availableCategories_isLoading.value = false;
-});
-
+      );
+    }
+    availableCategories_isLoading.value = false;
+  });
 </script>
 
-<template>index
-
+<template>
+  index
 
   <nuxt-layout name="centered-content">
-
     <div v-if="availableCategories && availableCategories.length > 0">
       <h2>Your Posts</h2>
       <div v-for="category in availableCategories" :key="category.categoryId">
         <p>{{ category.url }}</p>
-        <img :src="category.url" :alt="`Category `+  category.name"/>
+        <img :src="category.url" :alt="`Category ` + category.name" />
 
         <!--
                 <v-btn @click="router.push(`/post/${post.id}`)">View</v-btn>
@@ -48,8 +45,5 @@ onMounted(async () => {
     <div v-else>
       <p>You have no posts</p>
     </div>
-
   </nuxt-layout>
-
-
 </template>
