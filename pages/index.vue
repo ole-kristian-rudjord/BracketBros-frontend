198874c6e6b8da827bb3e28e366a4d2ea10be1b7
rasmusjs <rasmus.skramstad@gmail.com>
2023-11-24 15:45:06 +0100

    Added pictures from db

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,55 @@
 <script setup lang="ts">
-  useHead({
-    title: 'BracketBros',
-  });
+import {toast} from "vue3-toastify";
+import {defaultToastOptions} from "~/constants";
+
+useHead({
+  title: 'BracketBros',
+});
+
+const availableCategories = ref<category[]>([]);
+const availableCategories_isLoading = ref(false);
+
+
+onMounted(async () => {
+  // TODO: reroute user to login if not logged in
+
+  availableCategories_isLoading.value = true;
+
+  const categoriesData = await getAllCategories();
+  if (categoriesData) {
+    availableCategories.value = categoriesData;
+  } else {
+    toast.error(
+        'Error fetching categories from the database.',
+        defaultToastOptions.error
+    );
+  }
+  availableCategories_isLoading.value = false;
+});
+
 </script>
 
-<template>index</template>
+<template>index
+
+
+  <nuxt-layout name="centered-content">
+
+    <div v-if="availableCategories && availableCategories.length > 0">
+      <h2>Your Posts</h2>
+      <div v-for="category in availableCategories" :key="category.categoryId">
+        <p>{{ category.url }}</p>
+        <img :src="category.url" :alt="`Category `+  category.name"/>
+
+        <!--
+                <v-btn @click="router.push(`/post/${post.id}`)">View</v-btn>
+        -->
+      </div>
+    </div>
+    <div v-else>
+      <p>You have no posts</p>
+    </div>
+
+  </nuxt-layout>
+
+
+</template>
--- utils/getAllCategories.ts
+++ utils/getAllCategories.ts
@@ -1,6 +1,13 @@
 export default async () => {
-  return genericFetch({
+  const {data: categoriesData} = await genericFetch({
     method: 'GET',
     url: 'https://localhost:7205/api/Post/GetCategories',
   });
+
+
+  categoriesData.forEach((category: { url: string; }) => {
+    category.url = category.url.replace("../", "https://localhost:7205/");
+  });
+
+  return categoriesData;
 };
