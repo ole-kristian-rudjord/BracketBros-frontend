ccfbd2d6d2176128d6c1a30e2c6fed4bf6d0e542
ole-kristian-rudjord <s354564@oslomet.no>
2023-11-26 12:16:19 +0100

    Added introduction with categories and tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import { useDisplay } from 'vuetify/lib/framework.mjs';
   import { toast } from 'vue3-toastify';
   import { defaultToastOptions } from '~/constants';
 
@@ -6,42 +7,116 @@
     title: 'BracketBros',
   });
 
+  const display = useDisplay();
+
+  const numberOfCategoryCols = computed(() => {
+    return display.mdAndUp.value ? 4 : display.smAndUp.value ? 6 : 12;
+  });
+
   const availableCategories = ref<category[]>([]);
-  const availableCategories_isLoading = ref(false);
+  const availableTags = ref<tag[]>([]);
 
   onMounted(async () => {
-    availableCategories_isLoading.value = true;
-
-    const categoriesData = await getAllCategories();
-    if (categoriesData) {
-      availableCategories.value = categoriesData;
+    const getAllCategories_data = await getAllCategories();
+    if (getAllCategories_data) {
+      availableCategories.value = getAllCategories_data.sort(
+        (a: category, b: category) => a.name.localeCompare(b.name)
+      );
     } else {
       toast.error(
         'Error fetching categories from the database.',
         defaultToastOptions.error
       );
     }
-    availableCategories_isLoading.value = false;
+
+    const getAllTags_response = await getAllTags();
+    if (getAllTags_response.data) {
+      // @ts-ignore
+      availableTags.value = getAllTags_response.data.sort((a: tag, b: tag) =>
+        a.name.localeCompare(b.name)
+      );
+    } else {
+      toast.error(
+        'Error fetching tags from the database.',
+        defaultToastOptions.error
+      );
+    }
   });
 </script>
 
 <template>
-  index
+  <div
+    style="max-width: 1000px"
+    class="d-flex flex-column w-100 mx-auto px-4 py-16"
+  >
+    <div class="d-flex flex-column ga-6">
+      <h1 class="text-h4 font-weight-bold">
+        Introducing BracketBros:<br />Your Go-To Platform for All Things
+        Programming!
+      </h1>
 
-  <nuxt-layout name="centered-content">
-    <div v-if="availableCategories && availableCategories.length > 0">
-      <h2>Your Posts</h2>
-      <div v-for="category in availableCategories" :key="category.categoryId">
-        <p>{{ category.url }}</p>
-        <img :src="category.url" :alt="`Category ` + category.name" />
+      <ul class="text-h6">
+        <li>
+          💬 Dive deep into discussions on languages, frameworks, and tools.
+        </li>
+        <li>📂 Share your latest projects, code snippets, or challenges.</li>
+        <li>
+          👥 Collaborate with fellow developers to solve problems and learn new
+          techniques.
+        </li>
+        <li>📚 Curated content to keep you updated on industry trends.</li>
+      </ul>
 
-        <!--
-                <v-btn @click="router.push(`/post/${post.id}`)">View</v-btn>
-        -->
-      </div>
+      <p class="text-h6 font-weight-bold">
+        Join the BracketBros community now and elevate your coding game!
+      </p>
     </div>
-    <div v-else>
-      <p>You have no posts</p>
+
+    <v-divider class="my-12"></v-divider>
+
+    <h2 class="mb-6 text-h4 font-weight-bold">Categories</h2>
+    <v-row>
+      <v-col
+        v-for="category in availableCategories"
+        :cols="numberOfCategoryCols"
+      >
+        <category-card
+          :key="category.categoryId"
+          :category="category"
+        ></category-card>
+      </v-col>
+    </v-row>
+
+    <v-divider class="my-12"></v-divider>
+
+    <h2 class="mb-6 text-h4 font-weight-bold">Tags</h2>
+    <div class="d-flex flex-wrap ga-2">
+      <v-hover v-for="tag in availableTags" :key="tag.tagId">
+        <template v-slot:default="{ isHovering, props }">
+          <v-chip
+            v-bind="props"
+            size="x-large"
+            :to="`/posts?tagId=${tag.tagId}`"
+            class="tag-chip"
+            :class="isHovering ? 'hover' : ''"
+          >
+            {{ tag.name }}
+          </v-chip>
+        </template>
+      </v-hover>
     </div>
-  </nuxt-layout>
+  </div>
 </template>
+
+<style scoped lang="scss">
+  ul {
+    list-style: none;
+  }
+
+  .tag-chip {
+    transition: transform 100ms ease;
+    &.hover {
+      transform: scale(1.075);
+    }
+  }
+</style>
